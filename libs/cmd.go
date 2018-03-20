b348a6dc437cc72786ecfdb793c50aa6fdaa582f
lijian <jianli@pptv.com>
2018-03-20 13:30:17 +0800

    add tls mail

--- libs/cmd.go
+++ libs/cmd.go
@@ -1,31 +1,27 @@
 package libs
 
 import (
-	"os/exec"
-	"os"
-	"strings"
 	"bytes"
 	"errors"
+	"fmt"
+	"os"
+	"os/exec"
 )
 
-func Cmd(cmds string, cmdDir ...string) (res string,err error) {
+func Cmd(cmds string, cmdDir ...string) (res string, err error) {
 	if len(cmdDir) == 1 {
 		if err := os.Chdir(cmdDir[0]); err != nil {
 			return res, err
 		}
 	}
 
-	var (
-		cmd string
-		cmdArgs, args []string
-	)
-	cmdArgs = strings.Split(cmds, " ")
-	cmd = cmdArgs[0]
-	args = cmdArgs[1:]
-	if strings.Count(cmds, "'") == 2 {
-		args = rsyncArgs(cmdArgs[1:])
-	}
-	command := exec.Command(cmd, args...)
+	in := bytes.NewBuffer(nil)
+	command := exec.Command("sh")
+	command.Stdin = in
+	go func() {
+		in.WriteString(fmt.Sprintf("%s\n", cmds))
+		in.WriteString("exit\n")
+	}()
 	var stdout, stderr bytes.Buffer
 	command.Stdout = &stdout
 	command.Stderr = &stderr
@@ -38,31 +34,5 @@ func Cmd(cmds string, cmdDir ...string) (res string,err error) {
 		return res, err
 	}
 
-	return	stdout.String(), nil
-}
-
-// 处理命令行参数中有带双引号子参数的参数,比如rsync中:'-e ssh -p 22'，只能处理一对双引号
-func rsyncArgs(args []string) []string {
-	var (
-		n, m int
-		str string
-		args1, args2, args3 []string
-	)
-	for k, v := range args {
-		if strings.HasPrefix(v, "'") == true {
-			n = k
-			args1 = args[:n]
-		}
-		if strings.HasSuffix(v, "'") == true {
-			m = k + 1
-			args3 = args[m:]
-		}
-	}
-	args2 = args[n:m]
-	str = strings.Join(args2, " ")	//'-e ssh -p 22'
-	str = strings.Trim(str, "'") //-e ssh -p 22
-	args = args1
-	args = append(args, str)
-	args = append(args, args3...)
-	return args
+	return stdout.String(), nil
 }
