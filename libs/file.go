3fbf831954efe431ca6aaa32580ae7326f843891
lijian <jianli@pptv.com>
2018-05-09 19:24:10 +0800

    add file op

---
+++ libs/file.go
@@ -0,0 +1,53 @@
+package libs
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// CopyDir 复制文件夹及其子目录和文件
+func CopyDir(srcDir, destDir string) (err error) {
+	err = filepath.Walk(srcDir, func(srcDir string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+
+		if f.IsDir() {
+			dir := destDir + f.Name()
+			err = os.MkdirAll(dir, 0755)
+			if err != nil {
+				return err
+			}
+		} else {
+			// file := destDir + f.Name()
+		}
+		return nil
+	})
+
+	return nil
+}
+
+// CopyFile 复制单个文件
+func CopyFile(srcFile, destFile string) (err error) {
+	f, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	destf, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+
+	defer destf.Close()
+
+	_, err = io.Copy(destf, f)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
