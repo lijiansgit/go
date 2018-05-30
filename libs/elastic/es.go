bd11af3726a0da7229e0b2ecd92f6ab056fefb7f
lijian <jianli@pptv.com>
2018-05-30 10:38:37 +0800

    add es

---
+++ libs/elastic/es.go
@@ -0,0 +1,51 @@
+package elastic
+
+import (
+	"context"
+
+	"github.com/olivere/elastic"
+)
+
+// Elastic es 结构体
+type Elastic struct {
+	Addrs    []string
+	Client   *elastic.Client
+	PingInfo *elastic.PingResult
+	PingCode int
+	Ctx      context.Context
+	GetRes   *elastic.GetResult
+}
+
+// NewElastic 初始化es
+func NewElastic(addrs []string) (es *Elastic, err error) {
+	options := []elastic.ClientOptionFunc{elastic.SetURL(addrs...)}
+	client, err := elastic.NewClient(options...)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	return &Elastic{Addrs: addrs, Client: client, Ctx: ctx}, nil
+}
+
+// Ping 操作
+func (e *Elastic) Ping() (err error) {
+	for _, addr := range e.Addrs {
+		e.PingInfo, e.PingCode, err = e.Client.Ping(addr).Do(e.Ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Get 查询操作
+func (e *Elastic) Get(index, typ, id string) (res *elastic.GetResult, err error) {
+	res, err = e.Client.Get().Index(index).Type(typ).Id(id).Do(e.Ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
