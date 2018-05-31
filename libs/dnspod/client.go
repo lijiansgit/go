04b55bc8ac345f0349807ee6722cebadb4aec85a
lijian <jianli@pptv.com>
2018-06-01 00:25:21 +0800

    add dnspod record op

--- libs/dnspod/client.go
+++ libs/dnspod/client.go
@@ -1,48 +1,104 @@
+// dnspod api 相关操作
+// 官方api文档：https://www.dnspod.cn/docs/info.html#common-parameters
+
 package dnspod
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/tidwall/gjson"
 )
 
-// DNSPod 结构体
-type DNSPod struct {
+// Client 结构体
+type Client struct {
 	// token 完整的 API Token 是由 ID,Token 组合而成的，用英文的逗号分割
 	Token  string
-	Format string
 	Domain string
+	Format string
 }
 
-// NewDNSPod 新结构体
-func NewDNSPod(token string) *DNSPod {
-	return &DNSPod{
+// NewClient 新结构体
+func NewClient(token, domain string) *Client {
+	return &Client{
 		Token:  token,
+		Domain: domain,
 		Format: "json",
 	}
 }
 
 // SetFormat 设置数据返回格式，默认json, 支持json/xml
-// func (d *DNSPod) SetFormat(format string) {
-// 	d.format = format
+// TODO
+// func (c *Client) SetFormat(format string) {
+// 	c.format = format
 // }
 
-// GetRecordList 获取域名记录列表
-func (d *DNSPod) GetRecordList(domain string) (res string, err error) {
-	url := URL + RecordList
-	req := d.InitParams()
-	req.Add("domain", d.Domain)
-	res, err = d.HTTPPost(url, req)
+// SetDomain 设置请求的域名
+func (c *Client) SetDomain(domain string) {
+	c.Domain = domain
+}
+
+// GetRecords 获取域名记录列表,返回map[recordId][name]
+func (c *Client) GetRecords() (records map[int64]string, err error) {
+	records = make(map[int64]string)
+	req := c.InitParams()
+	res, err := c.GetRecordRawRes(req)
+	if err != nil {
+		return records, err
+	}
+
+	list := gjson.Get(res, "records").Array()
+	for _, v := range list {
+		id := v.Get("id").Int()
+		name := v.Get("name").String()
+		records[id] = name
+	}
+
+	return records, err
+}
+
+// GetRecordId 获取域名记录的id
+func (c *Client) GetRecordId(record string) (id int64, err error) {
+	req := c.InitParams()
+	req.Set("keyword", record)
+	res, err := c.GetRecordRawRes(req)
+	if err != nil {
+		return id, err
+	}
+
+	list := gjson.Get(res, "records").Array()
+	for _, v := range list {
+		id = v.Get("id").Int()
+		name := v.Get("name").String()
+		lineId := v.Get("line_id").String()
+		if name == record && lineId == "0" {
+			return id, nil
+		}
+	}
+
+	return id, ErrRecordNotExists
+}
+
+// GetRecordRawRes 获取记录的详细原始信息
+func (c *Client) GetRecordRawRes(req url.Values) (res string, err error) {
+	res, err = c.HTTPPost(RecordListURL, req)
 	if err != nil {
 		return res, err
+	}
+
+	resCode := gjson.Get(res, "status.code").Int()
+	if resCode != 1 {
+		return res, errors.New(res)
 	}
 
 	return res, err
 }
 
 // HTTPPost post请求
-func (d *DNSPod) HTTPPost(url string, req url.Values) (res string, err error) {
+func (c *Client) HTTPPost(url string, req url.Values) (res string, err error) {
 	resp, err := http.Post(url, ContentType, strings.NewReader(req.Encode()))
 	if err != nil {
 		return res, err
@@ -60,9 +116,10 @@ func (d *DNSPod) HTTPPost(url string, req url.Values) (res string, err error) {
 }
 
 // InitParams 初始化请求参数
-func (d *DNSPod) InitParams() url.Values {
+func (c *Client) InitParams() url.Values {
 	req := url.Values{}
-	req.Set("login_token", d.Token)
-	req.Add("format", d.Format)
+	req.Set("login_token", c.Token)
+	req.Set("domain", c.Domain)
+	req.Set("format", c.Format)
 	return req
 }
--- libs/dnspod/define.go
+++ libs/dnspod/define.go
@@ -1,5 +1,7 @@
 package dnspod
 
+import "errors"
+
 const (
 	// ContentType http内容类型
 	ContentType = "application/x-www-form-urlencoded"
@@ -7,4 +9,37 @@ const (
 	URL = "https://dnsapi.cn/"
 	// RecordList 记录列表api
 	RecordList = "Record.List"
+	// 记录列表URL
+	RecordListURL = URL + RecordList
 )
+
+var (
+	// errors
+	ErrRecordNotExists = errors.New("record is not exists")
+)
+
+// RecordLineToId 线路名称转为线路ID
+func RecordLineToId(recordLine string) string {
+	switch recordLine {
+	case "默认":
+		return "0"
+	case "国内":
+		return "7=0"
+	case "国外":
+		return "3=0"
+	case "电信":
+		return "10=0"
+	case "联通":
+		return "10=1"
+	case "教育网":
+		return "10=2"
+	case "移动":
+		return "10=3"
+	case "百度":
+		return "90=0"
+	case "谷歌":
+		return "90=1"
+	}
+
+	return "0"
+}
