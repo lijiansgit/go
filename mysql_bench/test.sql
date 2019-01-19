45a27dd6c6913090248a3f6a31f5ee0a8e8aa060
lijian02 <lijian02@qutoutiao.net>
2019-01-19 19:56:11 +0800

    tools mv github.com/lijiansgit/tools

--- mysql_bench/test.sql
+++
@@ -1,7 +0,0 @@
-select * from  test.dsns  where id=5;
-select * from  test.dsns  where id=5;
-select * from  test.dsns  where id=5;
-select * from  test.dsns  where id=5;
-select * from  test.dsns  where id=5;
-select * from  test.dsns  where id=5;
-select * from  test.dsnsa  where id=5;
--- release/auto_commit_version.sh
+++
@@ -1,4 +0,0 @@
-res=`awk '/Version:/{print $0}' main.go`
-ver=`echo ${res: -3:-2}`
-verNew=$(( $ver + 1 ))
-sed -i "/Version:/s/$ver/$verNew/g" main.go
